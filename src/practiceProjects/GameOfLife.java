21388e2cac9e3771ed77fe5349f1a5614cde0f29
Goatz <Goataragua@gmail.com>
2024-12-14 15:53:45 +0200

    Insert spaces for readability

--- src/practiceProjects/GameOfLife.java
+++ src/practiceProjects/GameOfLife.java
@@ -45,7 +45,7 @@ public class GameOfLife {
     public static void printGrid(int[][] grid) {
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
-                System.out.print(grid[i][j] == 1 ? "*" : ".");
+                System.out.print(grid[i][j] == 1 ? "* " : ". ");
             }
             System.out.println();
         }
